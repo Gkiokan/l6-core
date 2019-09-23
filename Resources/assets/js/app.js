4bc5c068a00fab8545022912d5535003901516d0
Gkiokan Sali <gkiokan@hotmail.de>
2019-09-24 00:52:25 +0200

    init l6-core package

---
+++ Resources/assets/js/app.js
@@ -0,0 +1,7 @@
+console.log('Testing new module partials')
+
+let el = document.createElement('div')
+el.style = "width: 100px; height: 100px; background: red"
+document.body.append(el)
+
+console.log('END.')
